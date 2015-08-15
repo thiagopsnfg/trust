be281dfb076f70faa722ad23afdcea2d48c2f357
thiagopsnfg <thiagopsnfg@gmail.com>
2015-08-15 10:34:18 -0300

    15/08/2015: Reorganização dos resources; Reorganização da navegação de clientes;

--- target/generated-sources/annotations/br/com/trust/model/Parcela_.java
+++ target/generated-sources/annotations/br/com/trust/model/Parcela_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-08-03T13:49:34")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-08-13T10:23:28")
 @StaticMetamodel(Parcela.class)
 public class Parcela_ { 
 
